a02da253bf07ac9d96fd5b0cec76dd745f937c9b
Shubham Srivastava <shubham@rytangle.com>
2020-01-14 16:59:32 +0530

    sample proj for android library and its implementation

---
+++ Loader/app/src/main/java/com/sdk/loader/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sdk.loader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.hello);
+        com.sdk.learner1.MainActivity m = new com.sdk.learner1.MainActivity();
+        textView.setText(m.getText());
+//        Intent intent = new Intent(this, com.sdk.learner1.MainActivity.class);
+//        intent.putExtra("com.sdk.learner1.MESSAGE", m.getText());
+//        startActivity(intent);
+    }
+}
